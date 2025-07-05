4c15110031809b98a089ee69a7c85e93bbccc6f9
sairamkumarm21 <sairamkumarm21@gmail.com>
2025-07-05 12:31:56 +0530

    Init

---
+++ src/main/java/com/freightfox_sairamkumarm/dispatch_load_balancer/DispatchLoadBalancerApplication.java
@@ -0,0 +1,13 @@
+package com.freightfox_sairamkumarm.dispatch_load_balancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchLoadBalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatchLoadBalancerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freightfox_sairamkumarm/dispatch_load_balancer/DispatchLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freightfox_sairamkumarm.dispatch_load_balancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchLoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
